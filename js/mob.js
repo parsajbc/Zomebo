e8287e6d73859faa0cc7f98f14eec77b04b6f639
parsahj <parsa.jadda@mail.utoronto.ca>
2022-05-17 00:51:56 -0400

    major change to mov move paths

--- js/mob.js
+++ js/mob.js
@@ -13,10 +13,143 @@ export function importMobImages() {
 
 class Mob {
     constructor() {
-        this.x = 7.5;
+        this.x = 13.5;
         this.y = 6.5;
+        this.spd = 0.03;
         this.img = mobImages[0][0];
+        this.pathX = [];
+        this.pathY = [];
     }
+
+    processpath() {
+        let xdir = 1;
+        if (this.x - mainPlayer.x > 1) xdir = -1;
+        let x = Math.abs(this.x - mainPlayer.x);
+        let ydir = 1;
+        if (this.y - mainPlayer.y > 1) ydir = -1;
+        let y = Math.abs(this.y - mainPlayer.y);
+        if (x <= 1 && y <= 1) {
+            if (this.characterDistance() <= 1) return;
+            else {
+                if (x >= y) {
+                    let rand = this.spd * xdir;
+                    this.pathX = [rand, rand, rand];
+                    this.pathY = [0, 0, 0];
+                    return;
+                } else {
+                    let rand = this.spd * ydir;
+                    this.pathY = [rand, rand, rand];
+                    this.pathX = [0, 0, 0];
+                    return;
+                }
+            }
+        }
+        if (x <= 1 || y <= 1) {
+            if (x > 1) {
+                let counter = 0;
+                let rand = this.spd * xdir;
+                this.pathX = [];
+                this.pathY = [];
+                while (x > 0 && counter < 33) {
+                    this.pathX.push(rand);
+                    this.pathY.push(0);
+                    x -= this.spd;
+                    counter++;
+                }
+                return;
+            }
+            if (y > 1) {
+                let counter = 0;
+                let rand = this.spd * ydir;
+                this.pathX = [];
+                this.pathY = [];
+                while (y > 0 && counter < 33) {
+                    this.pathY.push(rand);
+                    this.pathX.push(0);
+                    y -= this.spd;
+                    counter++;
+                }
+                return;
+            }
+        } else {
+            let rand = Math.random();
+            if (rand <= (1 / 3)) {
+                let counter = 0;
+                let rand = this.spd * ydir;
+                this.pathX = [];
+                this.pathY = [];
+                while (y > 0 && counter < 33) {
+                    this.pathY.push(rand);
+                    this.pathX.push(0);
+                    y -= this.spd;
+                    counter++;
+                }
+                return;
+            } if (rand <= (2 / 3)) {
+                let counter = 0;
+                let rand = this.spd * xdir;
+                this.pathX = [];
+                this.pathY = [];
+                while (x > 0 && counter < 33) {
+                    this.pathX.push(rand);
+                    this.pathY.push(0);
+                    x -= this.spd;
+                    counter++;
+                }
+                return;
+            } else {
+                let counter = 0;
+                let rand = Math.sqrt((this.spd ** 2) / 2);
+                let randX = rand * xdir;
+                let randY = rand * ydir;
+                this.pathX = [];
+                this.pathY = [];
+                while (x > 0 && counter < 33) {
+                    this.pathX.push(randX);
+                    this.pathY.push(randY);
+                    x -= this.spd;
+                    counter++;
+                }
+                return;
+            }
+        }
+    }
+
+    // processPath() {
+    //     let xdir = 1;
+    //     if (this.x - mainPlayer.x > 1) xdir = -1;
+    //     let x = Math.abs(this.x - mainPlayer.x);
+    //     let ydir = 1;
+    //     if (this.y - mainPlayer.y > 1) ydir = -1;
+    //     let y = Math.abs(this.y - mainPlayer.y);
+    //     if (x <= 1 && y <= 1) {
+    //         return;
+    //     }
+    //     if (x <= 1 || y <= 1) {
+    //         if (x > 1) {
+    //             let dir;
+    //             if (xdir == 1) { dir = 'r'; } else { dir = 'l'; }
+    //             while (x > 1) {
+    //                 this.path.push(dir);
+    //                 x -= this.spd;
+    //             }
+    //             return;
+    //         }
+    //         if (y > 1) {
+    //             let dir;
+    //             if (ydir == 1) { dir = 'd'; } else { dir = 'u'; }
+    //             while (y > 1) {
+    //                 this.path.push(dir);
+    //                 y -= this.spd;
+    //             }
+    //             return;
+    //         }
+    //     }
+    //     while (x > 2 && y > 2) { 
+    //         let rand = Math.random();
+    //     }
+    //     let xy = x - y;
+    // }
 
     characterDistance() {
         return Math.sqrt((this.x - mainPlayer.x) ** 2 + (this.y - mainPlayer.y) ** 2);
@@ -24,7 +157,32 @@ class Mob {
 
     updatePosition() {
         if (this.characterDistance() >= 1) {
-            return;
+            if (this.pathX.length == 0) {
+                this.processpath();
+            }
+
+            let xMovement = this.pathX.pop();
+            let yMovement = this.pathY.pop();
+
+            this.x += xMovement;
+            this.y += yMovement;
+
+            // let a = Math.abs(this.x - mainPlayer.x);
+            // let b = Math.abs(this.y - mainPlayer.y);
+            // console.log(a, b);
+
+            // if (a > 1 && b <= 1) {
+            //     this.x -= this.spd * (this.x - mainPlayer.x) / a;
+            // } else if (a <= 1 && b > 1) {
+            //     this.y -= this.spd * (this.y - mainPlayer.y) / b;
+            // } else {
+            //     let c = Math.random();
+            //     if (c > 0.5) {
+            //         this.x -= this.spd * (this.x - mainPlayer.x) / a;
+            //     } else {
+            //         this.y -= this.spd * (this.y - mainPlayer.y) / b;
+            //     }
+            // }
         } else {
             let pushDistance = (1 - this.characterDistance()) * 0.2;
             let angle = Math.atan((this.y - mainPlayer.y) / (this.x - mainPlayer.x));
